73f53d78605d2e94ec859753d9aee002b11701bb
Sharon <shar1.v.kori@gmail.com>
2025-02-05 16:49:21 +0100

    on welcomepage : worked on img and line height

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -2,9 +2,10 @@ import { Link } from "react-router-dom";
 
 
 export const Welcome = () => {
-  return <div className="p-8">
-    <div className="bg-[url('/img/welcome_page.png')] bg-cover  h-screen w-full mb-12 flex items-center justify-center">
-      <p className="light-cream text-3xl px-12 py-6">Hi Leon, <br /> welcome <br />
+  return <div className="p-4 mx-auto">
+    <div className="bg-[url('/img/welcome_page.png')] bg-contain bg-no-repeat  h-screen w-full mb-8 flex   relative">
+    <h3 className="light-cream text-center text-xl tracking-widest absolute inset-x-0 top-8 z-1 ">SILENT MOON</h3>
+      <p className="light-cream text-3xl/normal tracking-wider font-extrabold pl-8 pt-28">Hi Leon, <br /> welcome <br />
       to Silent <br /> Moon</p>
     </div>
     <Link to={'/home'} className="btn-pink">GET STARTED</Link>
