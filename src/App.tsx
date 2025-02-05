561e6b8fdc80807dc9d22faf13c38989ad6efa63
Sharon <shar1.v.kori@gmail.com>
2025-02-05 13:42:16 +0100

    added btn-pink class to index.css, started welcomepage

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ import { Welcome } from './pages/welcome/Welcome';
 import { Home } from './pages/home/Home';
 import { PageDetail } from './pages/pageDetail/PageDetail';
 import { Music } from './pages/music/Music';
-ihttps://github.com/FranticMario/silent-moon/pull/2/conflict?name=src%252Fpages%252Flogin%252FLogin.tsx&ancestor_oid=0a4793a7dde518709ce953771b9b56f3be5b731a&base_oid=cc65acd2b61662040a0175c8d9daa37628d4a8de&head_oid=2a4c5aa59de68d553faa57e7fbb2e8defd2c3041mport { Profile } from './pages/profile/Profile';
 import { Reminders } from './pages/reminders/Reminders';
 import { MeditationPlayer } from './pages/meditationPlayer/MeditationPlayer';
 import { Activity } from './pages/activity/Activity';
 
 import SignIn from './pages/signIn/SignIn';
 import SignUp from './pages/signUp/SignUp';
+import { Profile } from './pages/profile/Profile';
 
 
 function App() {
--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -1,5 +1,13 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+
 
 export const Welcome = () => {
-  return <div>Welcome</div>;
+  return <div className="p-8">
+    <div className="bg-[url('/img/welcome_page.png')] bg-cover  h-screen w-full mb-12 flex items-center justify-center">
+      <p className="light-cream text-3xl px-12 py-6">Hi Leon, <br /> welcome <br />
+      to Silent <br /> Moon</p>
+    </div>
+    <Link to={'/home'} className="btn-pink">GET STARTED</Link>
+  </div>;
 };
+
