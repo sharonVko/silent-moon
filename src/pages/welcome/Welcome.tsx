4c0d47ba34451daa750f5fa5817ad53446e95fc4
Sharon <shar1.v.kori@gmail.com>
2025-02-05 17:16:54 +0100

    in index.html: changed title and logo for tab

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export const Welcome = () => {
   return <div className="p-4 mx-auto">
-    <div className="bg-[url('/img/welcome_page.png')] bg-contain bg-no-repeat  h-screen w-full mb-8 flex   relative">
+    <div className="bg-[url('/img/welcome_page.png')] bg-contain bg-no-repeat  h-screen w-full mb-8 flex relative">
     <h3 className="light-cream text-center text-xl tracking-widest absolute inset-x-0 top-8 z-1 ">SILENT MOON</h3>
       <p className="light-cream text-3xl/normal tracking-wider font-extrabold pl-8 pt-28">Hi Leon, <br /> welcome <br />
       to Silent <br /> Moon</p>
