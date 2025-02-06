02ebd6b46e4fb74b6c0d2bfe7573297f949fab8c
Sharon <shar1.v.kori@gmail.com>
2025-02-06 15:53:00 +0100

    installed react h5 audio player, added imgs for player buttons

--- src/pages/meditationPlayer/MeditationPlayer.tsx
+++ src/pages/meditationPlayer/MeditationPlayer.tsx
@@ -3,26 +3,45 @@ import { Backbutton } from '../../components/backButton/Backbutton';
 import { DownloadButton } from '../../components/downloadButton/DownloadButton';
 import supabase from '../../utils/supabase';
 import { useParams } from 'react-router-dom';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 export const MeditationPlayer = () => {
-  const [musik, setMusik] = useState('');
+  const [music, setMusic] = useState('');
   const { id } = useParams();
+
   useEffect(() => {
-    const fetchMusik = async () => {
-      const { data, error } = await supabase.from('playlist_tracks').select('*').eq('id', id).single();
+    const fetchMusic = async () => {
+      const { data, error } = await supabase
+      .from('playlist_tracks')
+      .select('*')
+      .eq('id', id)
+      .single();
 
       if (data) {
-        setMusik(data);
+        setMusic(data);
       }
     };
 
-    fetchMusik();
-  }, []);
+    fetchMusic();
+  }, [id]);
 
   return (
-    <div className="relative">
+    <div className="relative  ">
+      <img src="/img/player_meditation.png" alt="light camouflage img" />
       <Backbutton />
-      <DownloadButton musik={musik?.track_url} />
+      <DownloadButton musik={music?.track_url} />
+      <div className=' player-container absolute top-80 left-18'>
+      <AudioPlayer
+    src={music}
+    onPlay={() => console.log('onPlay')}
+    showSkipControls={true}
+    showJumpControls={true}
+    customAdditionalControls={[]}
+    customVolumeControls={[]}
+  />
+      </div>
+
     </div>
   );
 };
