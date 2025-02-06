103157b1a6da1be18ebf09085f349da5990e608d
Sharon <shar1.v.kori@gmail.com>
2025-02-06 11:33:47 +0100

    minor changes in welcomepage

--- src/pages/welcome/Welcome.tsx
+++ src/pages/welcome/Welcome.tsx
@@ -9,8 +9,8 @@ export const Welcome = () => {
     <div className="bg-[url('/img/welcome_page.png')] bg-contain bg-no-repeat h-screen w-full mb-12">
     </div>
     <h3 className="light-cream text-center text-[16px] font-medium tracking-widest absolute inset-x-0 top-8 z-1 ">SILENT MOON</h3>
-      <p className="light-cream text-3xl/normal tracking-wider font-extrabold pl-8 pt-2 absolute top-24 left-1">Hi USER , <br /> welcome <br />
+      <p className="sans-pro-900 light-cream text-[36px]/normal tracking-wider font-extrabold pl-4 pt-2 absolute top-24 left-1">Hi USER , <br /> welcome <br />
       to Silent <br /> Moon</p>
-    <Link to={'/home'} className="btn-pink absolute bottom-18 left-8">GET STARTED</Link>
+    <Link to={'/home'} className="btn-pink light-cream absolute bottom-24 left-8 text-center pt-4">GET STARTED</Link>
   </div>;
 };
