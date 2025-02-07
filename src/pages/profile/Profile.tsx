c4d9265293d9d0552bcfc8f2cd39c68517a8f529
griez97 <bryniarski.daniel@gmail.com>
2025-02-07 10:41:09 +0100

    Profile Changes

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,42 +4,56 @@ import supabase from "../../utils/supabase";
 import { Footer } from "../../components/footer/Footer";
 import { useUserContext } from "../../context/UserProvider";
 import { Search } from "../../components/search/Search";
-// import FavouriteList from "../../components/favouriteList/favouriteList";
+import { Meditation, Yoga } from "../home/Home";
+import { ArticleList } from "../../components/articleList/ArticleList";
+import UserAvatar from "../../components/userAvatar/UserAvatar";
 
 
 
 const Profile = () => {
 
   const { user } = useUserContext()
-  const [favouriteYoga, setFavouriteYoga] = useState([])
-  const [favouriteMeditation, setFavouriteMeditation] = useState([])
+  const [favouriteYoga, setFavouriteYoga] = useState<Yoga[]>([])
+  const [favouriteMeditation, setFavouriteMeditation] = useState<Meditation[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
-      const {data: favYoga, error: yogaErr} = await supabase.from('user_favotires_yoga').select('*').eq('user_id', user?.id)
-    
-      if(yogaErr) console.error(yogaErr);
-      
-      const { data: favMedidation, error: meditationErr} = await supabase.from('user_favorites_meditation').select('*').eq('user_id' , user?.id)
+      if(user?.id){
+        const {data: yogaResult, error: yogaError} = await 
+          supabase
+          .from('user_favorites_yoga')
+          .select('*')
+          .eq('user_id', user.id);
+          
+        const {data: mediResult, error: mediError} = await
+          supabase
+          .from('user_favorites_meditation')
+          .select('*')
+          .eq('user_id', user.id)
+        
+        if(yogaResult) setFavouriteYoga(yogaResult)
+          else console.error(yogaError);
 
-      if(meditationErr) console.error(meditationErr);
-      
-      setFavouriteYoga(favYoga)
-      setFavouriteMeditation(favMedidation)
+        if(mediResult) setFavouriteMeditation(mediResult)
+          else console.error(mediError);
+      }
     }
-    fetchData()
-  }, [user?.id])
 
+    fetchData()
+  }, [user])
+
+  console.log(user);
   console.log(favouriteMeditation);
   console.log(favouriteYoga);
-  
-  
 
   return ( 
     <>
       <Header/>
+      <div>
+        <UserAvatar/>
+      </div>
       <Search/>
-      {/* <FavouriteList yoga={favouriteYoga} meditation={favouriteMeditation}/> */}
+      <ArticleList yoga={favouriteYoga} meditation={favouriteMeditation}/>
       <Footer/>
     </>
   );
