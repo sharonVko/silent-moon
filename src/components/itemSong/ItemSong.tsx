2e1d155df411cbb67ec959cf10156b01c848e3fa
Sharon <shar1.v.kori@gmail.com>
2025-02-10 17:16:12 +0100

    in ItemSong: changes in structure, added hr to seperate tracks

--- src/components/itemSong/ItemSong.tsx
+++ src/components/itemSong/ItemSong.tsx
@@ -1,14 +1,34 @@
+/* import { PlayIcon } from '../../assets/svg/PlayIcon';
+import { Link } from 'react-router-dom';
+
+export const ItemSong = ({ id, title, duration }: { id: number; title: string; duration: string }) => {
+  return (
+    <Link to={`/player/${id}`} className="flex gap-5 py-2">
+      <PlayIcon />
+      <div className="">
+        <p className="font-semibold text-[16px]">{title}</p>
+        <p className="font-semibold text-[11px]">{duration}</p>
+      </div>
+      <hr className="border-t border-gray-300 my-2 grey"/>
+    </Link>
+  );
+};
+ */
+
 import { PlayIcon } from '../../assets/svg/PlayIcon';
 import { Link } from 'react-router-dom';
 
 export const ItemSong = ({ id, title, duration }: { id: number; title: string; duration: string }) => {
   return (
-    <Link to={`/player/${id}`} className="flex gap-5">
+    <article className='pr-7'>
+    <Link to={`/player/${id}`} className="flex gap-5 py-3">
       <PlayIcon />
       <div className="">
         <p className="font-semibold text-[16px]">{title}</p>
         <p className="font-semibold text-[11px]">{duration}</p>
       </div>
     </Link>
+      <hr className="border-t border-gray-100 my-2"/>
+    </article>
   );
 };
