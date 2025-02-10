ae339943b16639c4190d7858c985d05483907bb6
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-02-10 15:57:22 +0100

    fix: add logout and firstname and remind me btn

---
+++ src/assets/svg/Logout.tsx
@@ -0,0 +1,26 @@
+export const Logout = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40" height="40">
+      <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+      <g id="SVGRepo_iconCarrier">
+        {' '}
+        <path d="M21 12L13 12" stroke="#4A503D" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />{' '}
+        <path
+          d="M18 15L20.913 12.087V12.087C20.961 12.039 20.961 11.961 20.913 11.913V11.913L18 9"
+          stroke="#4A503D"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />{' '}
+        <path
+          d="M16 5V4.5V4.5C16 3.67157 15.3284 3 14.5 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H14.5C15.3284 21 16 20.3284 16 19.5V19.5V19"
+          stroke="#323232"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />{' '}
+      </g>
+    </svg>
+  );
+};
