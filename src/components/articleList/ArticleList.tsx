62506d90cc31273963ab0763e5fc9981dd4d9c5b
Andrej Dortmann <andrej.dortmann25@gmail.com>
2025-02-11 20:29:34 +0100

    fix: fix text

--- src/components/articleList/ArticleList.tsx
+++ src/components/articleList/ArticleList.tsx
@@ -10,12 +10,18 @@ import { FreeMode, Pagination } from 'swiper/modules';
 import { ArticleCard } from '../articleCard/ArticleCard';
 import { Meditation, Yoga } from '../../pages/home/Home';
 import { Loader } from '../loader/Loader';
+import { useLocation } from 'react-router-dom';
 
 export const ArticleList = ({ yoga, meditation }: { yoga: Yoga[]; meditation: Meditation[] }) => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <div className="recomendet__yoga">
-        <h2 className="font-black text-[24px] text-[#4A503D] mb-6">Recomended Yoga for you</h2>
+        <h2 className="font-black text-[24px] text-[#4A503D] mb-6">
+          {' '}
+          {pathname === '/home' ? 'Recommended Yoga for you' : 'Favourite Yoga Sessions'}
+        </h2>
 
         <Swiper
           slidesPerView={2}
@@ -23,8 +29,10 @@ export const ArticleList = ({ yoga, meditation }: { yoga: Yoga[]; meditation: Me
           freeMode={true}
           modules={[FreeMode, Pagination]}
           className="mySwiper">
-          {!yoga || yoga.length === 0 ? (
+          {!yoga ? (
             <Loader />
+          ) : yoga.length === 0 ? (
+            <p>{pathname === '/home' ? <Loader /> : ' No Favourite Yoga Available'}</p>
           ) : (
             yoga.map(yogaSingle => (
               <SwiperSlide key={yogaSingle.id}>
@@ -33,25 +41,30 @@ export const ArticleList = ({ yoga, meditation }: { yoga: Yoga[]; meditation: Me
             ))
           )}
         </Swiper>
-      </div>
-      <div className="recomendet__meditation">
-        <h2 className="font-black text-[24px] text-[#4A503D] mb-6">Recomended Meditation for you</h2>
-        <Swiper
-          slidesPerView={2}
-          spaceBetween={20}
-          freeMode={true}
-          modules={[FreeMode, Pagination]}
-          className="mySwiper">
-          {!meditation || meditation.length === 0 ? (
-            <Loader />
-          ) : (
-            meditation.map(meditationSingle => (
-              <SwiperSlide key={meditationSingle.id}>
-                <ArticleCard meditationSingle={meditationSingle} />
-              </SwiperSlide>
-            ))
-          )}
-        </Swiper>
+
+        <div className="recomendet__meditation">
+          <h2 className="font-black text-[24px] text-[#4A503D] mb-6">
+            {pathname === '/home' ? 'Recommended Meditation for you' : 'Favourite Meditation '}
+          </h2>
+          <Swiper
+            slidesPerView={2}
+            spaceBetween={20}
+            freeMode={true}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper">
+            {!meditation ? (
+              <Loader />
+            ) : meditation.length === 0 ? (
+              <p>{pathname === '/home' ? <Loader /> : ' No Favourite Meditation Available'}</p>
+            ) : (
+              meditation.map(meditationSingle => (
+                <SwiperSlide key={meditationSingle.id}>
+                  <ArticleCard meditationSingle={meditationSingle} />
+                </SwiperSlide>
+              ))
+            )}
+          </Swiper>
+        </div>
       </div>
     </>
   );
